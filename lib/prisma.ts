e8e16d2d243dbe2165de85ff49aad12f3a78b3e6
flashcode635 <151376451+flashcode635@users.noreply.github.com>
2026-04-16 20:58:16 +0530

    prisma issue solved

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -9,12 +9,10 @@
 
 // if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 import "dotenv/config";
+import { PrismaClient } from "@/app/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
-
-export { prisma };
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+export const prisma = new PrismaClient({ adapter });
